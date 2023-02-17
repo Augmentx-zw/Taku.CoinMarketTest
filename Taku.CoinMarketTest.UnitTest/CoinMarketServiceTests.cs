0b4c62b2e3f5f5145bbc94e45d5a1d0948801562
Takudzwa A Vafana <vafanat@gmail.com>
2023-02-17 09:43:23 +0200

    Added Unit tests

---
+++ Taku.CoinMarketTest.UnitTest/CoinMarketServiceTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Extensions.Options;
+using Moq;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using Taku.CoinMarketTest.Domain;
+using Taku.CoinMarketTest.Domain.Configurations;
+using Taku.CoinMarketTest.Domain.DomainEntities;
+using Taku.CoinMarketTest.Domain.DTO.IntegrationDto;
+using Taku.CoinMarketTest.Domain.Services;
+using Xunit;
+
+namespace Taku.CoinMarketTest.UnitTest
+{
+    public class CoinMarketServiceTests
+    {
+        private readonly Mock<IHttpService> _httpServiceMock;
+        private readonly Mock<IRepository<ExchangeRate>> _repositoryMock;
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+
+        public CoinMarketServiceTests()
+        {
+            _httpServiceMock = new Mock<IHttpService>();
+            _repositoryMock = new Mock<IRepository<ExchangeRate>>();
+            _unitOfWorkMock = new Mock<IUnitOfWork>();
+        }
+
+        [Fact]
+        public async Task GetCoinRequestAsync_Returns_CoinClassDto()
+        {
+            // Arrange
+            var options = Options.Create(new AppConfigs
+            {
+                Key = "fakeApiKey",
+                Limit = "100"
+            });
+            var service = new CoinMarketService(options, _httpServiceMock.Object, _repositoryMock.Object, _unitOfWorkMock.Object);
+
+            var currency = "USD";
+            var expected = new CoinClassDto();
+
+            _httpServiceMock.Setup(x => x.GetAsync(It.IsAny<string>(), It.IsAny<List<KeyValuePair<string, string>>>()))
+                .ReturnsAsync(JsonConvert.SerializeObject(expected));
+
+            // Act
+            var actual = await service.GetCoinRequestAsync(currency);
+
+            // Assert
+            Assert.IsType<CoinClassDto>(actual);
+        }
+
+        [Fact]
+        public async Task AddCoinMarketDataAsync_Stores_ExchangeRate()
+        {
+            // Arrange
+            var service = new CoinMarketService(Options.Create(new AppConfigs()), _httpServiceMock.Object, _repositoryMock.Object, _unitOfWorkMock.Object);
+
+            var coinClassDto = new CoinClassDto();
+            var expected = new ExchangeRate();
+
+            _repositoryMock.Setup(x => x.InsertAsync(It.IsAny<ExchangeRate>()))
+                .Returns(Task.CompletedTask);
+
+            _unitOfWorkMock.Setup(x => x.SaveAsync())
+                .Returns(Task.CompletedTask);
+
+            // Act
+            await service.AddCoinMarketDataAsync(coinClassDto);
+
+            // Assert
+            _repositoryMock.Verify(x => x.InsertAsync(It.IsAny<ExchangeRate>()), Times.Once);
+            _unitOfWorkMock.Verify(x => x.SaveAsync(), Times.Once);
+        }
+    }
+}
---
+++ Taku.CoinMarketTest.UnitTest/GetExchangeRateQueryHandlerTests.cs
@@ -0,0 +1,44 @@
+﻿using Moq;
+using Taku.CoinMarketTest.Domain.DTO.IntegrationDto;
+using Taku.CoinMarketTest.Domain.QueryHandlers.ExchangeRateDetails;
+using Taku.CoinMarketTest.Domain.Services;
+
+namespace Taku.CoinMarketTest.UnitTest
+{
+    public class GetExchangeRateQueryHandlerTests
+    {
+        [Fact]
+        public async Task Handle_WithCurrency_ReturnsCoinClassDto()
+        {
+            // Arrange
+            var expectedCoinClassDto = new CoinClassDto();
+            var mockCoinMarketService = new Mock<ICoinMarketService>();
+            mockCoinMarketService.Setup(x => x.GetCoinRequestAsync(It.IsAny<string>()))
+                .ReturnsAsync(expectedCoinClassDto);
+            var query = new GetExchangeRateQuery { Currency = "USD" };
+            var handler = new GetExchangeRateQueryHandler(mockCoinMarketService.Object);
+
+            // Act
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            Assert.Equal(expectedCoinClassDto, result);
+        }
+
+        [Fact]
+        public async Task Handle_WithCurrency_CallsCoinMarketService()
+        {
+            // Arrange
+            var mockCoinMarketService = new Mock<ICoinMarketService>();
+            var query = new GetExchangeRateQuery { Currency = "USD" };
+            var handler = new GetExchangeRateQueryHandler(mockCoinMarketService.Object);
+
+            // Act
+            await handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            mockCoinMarketService.Verify(x => x.GetCoinRequestAsync(query.Currency), Times.Once);
+            mockCoinMarketService.Verify(x => x.AddCoinMarketDataAsync(It.IsAny<CoinClassDto>()), Times.Once);
+        }
+    }
+}
