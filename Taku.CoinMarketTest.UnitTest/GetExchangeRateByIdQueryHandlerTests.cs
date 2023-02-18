d9c7d5601828b63904407542250fd0d63c22c31f
Takudzwa A Vafana <vafanat@gmail.com>
2023-02-18 19:51:48 +0200

    added new test

---
+++ Taku.CoinMarketTest.UnitTest/GetExchangeRateByIdQueryHandlerTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+using Taku.CoinMarketTest.Domain.QueryHandlers.ExchangeRateDetails;
+using Taku.CoinMarketTest.Domain.DomainEntities;
+using Taku.CoinMarketTest.Domain;
+
+public class GetExchangeRateByIdQueryHandlerTests
+{
+    [Fact]
+    public async Task Handle_ValidExchangeRateId_ReturnsExchangeRate()
+    {
+        // Arrange
+        var exchangeRateId = Guid.NewGuid();
+        var exchangeRate = new ExchangeRate { ExchangeRateId = exchangeRateId };
+        var repositoryMock = new Mock<IRepository<ExchangeRate>>();
+        repositoryMock.Setup(repo => repo.GetByIDAsync(exchangeRateId)).ReturnsAsync(exchangeRate);
+        var query = new GetExchangeRateByIdQuery { ExchangeRateId = exchangeRateId };
+        var handler = new GetExchangeRateByIdQueryHandler(repositoryMock.Object);
+
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        Assert.Equal(exchangeRate, result);
+    }
+
+    [Fact]
+    public async Task Handle_InvalidExchangeRateId_ReturnsNull()
+    {
+        // Arrange
+        var exchangeRateId = Guid.NewGuid();
+        var repositoryMock = new Mock<IRepository<ExchangeRate>>();
+        repositoryMock.Setup(repo => repo.GetByIDAsync(exchangeRateId)).ReturnsAsync(null as ExchangeRate);
+        var query = new GetExchangeRateByIdQuery { ExchangeRateId = exchangeRateId };
+        var handler = new GetExchangeRateByIdQueryHandler(repositoryMock.Object);
+
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        Assert.Null(result);
+    }
+}
